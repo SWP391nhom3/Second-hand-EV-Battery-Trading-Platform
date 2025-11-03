f417bdadf9fde48039b79adedf198701c4cab92d
juantran_22 <tranminhquan564@gmail.com>
2025-11-03 14:19:14 +0700

    chore(migrations): switch to EF-native AddColumn/DropColumn for ImageUrl (no IF EXISTS); rely on db.Database.Migrate() to force update

--- EVehicleManagementAPI/EVehicleManagementAPI/Migrations/20251103001000_AddImageUrlToModels.cs
+++ EVehicleManagementAPI/EVehicleManagementAPI/Migrations/20251103001000_AddImageUrlToModels.cs
@@ -6,51 +6,28 @@ namespace EVehicleManagementAPI.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            // VehicleModels.ImageUrl
-            migrationBuilder.Sql(@"
-IF NOT EXISTS (
-    SELECT 1 FROM sys.columns 
-    WHERE Name = N'ImageUrl' AND Object_ID = Object_ID(N'dbo.VehicleModels')
-)
-BEGIN
-    ALTER TABLE dbo.VehicleModels ADD ImageUrl NVARCHAR(512) NULL;
-END
-");
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "VehicleModels",
+                type: "nvarchar(512)",
+                nullable: true);
 
-            // BatteryModels.ImageUrl
-            migrationBuilder.Sql(@"
-IF NOT EXISTS (
-    SELECT 1 FROM sys.columns 
-    WHERE Name = N'ImageUrl' AND Object_ID = Object_ID(N'dbo.BatteryModels')
-)
-BEGIN
-    ALTER TABLE dbo.BatteryModels ADD ImageUrl NVARCHAR(512) NULL;
-END
-");
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "BatteryModels",
+                type: "nvarchar(512)",
+                nullable: true);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            // Xóa cột nếu tồn tại (an toàn khi rollback)
-            migrationBuilder.Sql(@"
-IF EXISTS (
-    SELECT 1 FROM sys.columns 
-    WHERE Name = N'ImageUrl' AND Object_ID = Object_ID(N'dbo.VehicleModels')
-)
-BEGIN
-    ALTER TABLE dbo.VehicleModels DROP COLUMN ImageUrl;
-END
-");
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "VehicleModels");
 
-            migrationBuilder.Sql(@"
-IF EXISTS (
-    SELECT 1 FROM sys.columns 
-    WHERE Name = N'ImageUrl' AND Object_ID = Object_ID(N'dbo.BatteryModels')
-)
-BEGIN
-    ALTER TABLE dbo.BatteryModels DROP COLUMN ImageUrl;
-END
-");
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "BatteryModels");
         }
     }
 }
